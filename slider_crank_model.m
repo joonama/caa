41420df22adb4f2c8b0ac8d4ba2b6af84764fe3f
Joona Marttila <joonama>
2022-11-30 15:17:56 +0200

    define bodies for slider crank

--- slider_crank_model.m
+++ slider_crank_model.m
@@ -8,9 +8,16 @@ sys = make_system();
 % location (x, y) and rotation (phi), name
 % q = [x_i, y_i, phi_i]' % generalized coordinates
 
-%structure best for containing information
+sys = add_body(sys, "ground");
+sys = add_body(sys, "crank", [-0.1, 0.05], -deg2rad(30));
+sys = add_body(sys, "link", [-0.5, 0.05], deg2rad(15));
+sys = add_body(sys, "slider", [-0.7, 0]);
+
+% Joints - kinematic (revolute and simple)
 
 
-body1 = struct("name", "ground");
-body1.location = [0, 0]';
-body1.orientation = 0;
+
+
+% body1 = struct("name", "ground");
+% body1.location = [0, 0]';
+% body1.orientation = 0;
